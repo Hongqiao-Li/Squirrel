f78a407f873b272273912a4b0d87a5a771e9759d
Jiaqi Zhang <81305592+zhang17-zj@users.noreply.github.com>
2021-04-14 16:26:11 +0800

    Update views.py

--- map/views.py
+++ map/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from sightings.models import SqurInfo
 
 
@@ -9,4 +8,3 @@ from sightings.models import SqurInfo
 def map(request):
     context = {"sightings": SqurInfo.objects.all()[:100]}
     return render(request, 'map/mapshow.html', context)
-
